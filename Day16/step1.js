4fab270c0ef7d2e9a8f7d8dc5982c04f268b2cd3
[gusdl1992] <[tlgusdl92@gmail.com]>
2023-12-15 14:03:06 +0900

    20231215

--- Day16/step1.js
+++ Day16/step1.js
@@ -5,3 +5,12 @@ const 윗몸일으키기목록 = ["10","20"];    // 윗몸일으키기 저장
 const 턱걸이목록 = ["10","20"];         // 턱걸이 저장
 const 총점목록 = ["30","60"];           // 총점 저장
 const 등급목록 = ["C","B"];             // 등급저장
+
+
+console.log(이름목록);
+console.log(팔굽혀펴기목록);
+console.log(윗몸일으키기목록);
+console.log(턱걸이목록);
+console.log(총점목록);
+console.log(등급목록);
+
